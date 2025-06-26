3637fd34356622e9057c53cf4a01926c57b034d1
Dake Peng <pengd@carleton.edu>
2025-06-26 00:04:29 -0500

    added header

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import ProjectCarousel from './ProjectCarousel.jsx'
 import Interests from './Interests.jsx'
 import SectionLine from './SectionLine.jsx'
+import Header from './Header.jsx'
 function App() {
   return (
     <div>
+      <Header />
       <SectionLine />
+      <Interests /> 
       <ProjectCarousel {...ProjectCarousel.defaultProps} />
       {/* <ProjectCard {...ProjectCard.defaultProps}/> */}
-      <Interests /> 
+
     </div>
   )
 }
