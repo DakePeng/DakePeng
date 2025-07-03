ff80885f79f733281cda344480297deaf11824b8
Dake Peng <pengd@carleton.edu>
2025-07-02 23:30:31 -0500

    rebuilding for render

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -25,7 +25,7 @@ class App extends React.Component {
     };
   }
   componentDidMount() {
-    const url = "http://ec2-3-143-219-82.us-east-2.compute.amazonaws.com:5000"
+    const url = "https://dakepeng-backend.onrender.com/"
     const url_experience = url + '/api/experiences';
     axios.get(url_experience)
       .then((response) => {
