20983c05d34e8b24d38a3413e96130180d87a655
Dake Peng <pengd@carleton.edu>
2025-07-07 21:11:39 -0500

    modularized FloatingMenu

--- frontend/src/components/Home/SectionHeader.jsx
+++ frontend/src/components/Home/SectionHeader.jsx
@@ -1,9 +1,9 @@
 export default function SectionHeader({title, description}) {
     return(
         <div className="text-center mb-12">
-            <h2 className="text-5xl font-extrabold text-center mb-12">
+            <h1 className="text-5xl font-extrabold text-center mb-12">
                 {title}
-            </h2>
+            </h1>
             <p className="text-center max-w-3xl mx-auto mb-12">
                 {description}
             </p>
