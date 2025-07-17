e3645d5df6469fb9910e472f4b2b39c2df0d8543
Dake Peng <pengd@carleton.edu>
2025-07-16 23:53:48 -0500

    removed blog

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,8 +1,6 @@
 import Home from './Home/Home.jsx'
-import BlogPost from './Blog/BlogPost.jsx'
 export default function (){
   return (
   <Home />
-  //<BlogPost {... BlogPost.defaultProps}/>
   );
 }
